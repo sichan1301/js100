066a6b4422c5743a14a45c9fda115b1813ec599a
sichan <sichan1301@naver.com>
2022-01-30 23:21:31 +0900

    1번 문제 복습

--- 1.js
+++ 1.js
@@ -1,8 +1,8 @@
 // 다음 배열에서 400,500을 삭제하는 code를 입력하세요.
 
-var nums =[100, 200, 300, 400, 500];
+var numbers =[100, 200, 300, 400, 500];
 
-nums.pop();
-nums.pop();
+numbers.pop();
+numbers.pop();
 
-console.log(nums);
+console.log(numbers);
