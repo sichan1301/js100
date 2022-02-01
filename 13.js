991a6b0e714cbfec08f519ba3b8cbcf6216e15eb
sichan <sichan1301@naver.com>
2022-02-01 19:57:58 +0900

    13번 추가

---
+++ 13.js
@@ -0,0 +1,13 @@
+const level = 5;
+
+
+for(let i =1;i<=level;i++){
+    let tree = "";
+    for(let k = 0;k<level-i;k++){
+        tree = tree + " "
+    }
+    for(let j = 1; j<=i*2-1;j++){
+        tree = tree + "*";
+    }
+    console.log(tree);
+}
