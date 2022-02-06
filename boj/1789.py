60c30be18275b9eeb8c2a06b6bcafce19792fbfc
sichan <sichan1301@naver.com>
2022-02-06 15:45:29 +0900

    boj 1789 추가

--- boj/1789.py
+++ boj/1789.py
@@ -1,11 +1,8 @@
 import sys
-
 n = int(sys.stdin.readline())
-list = []
-for i in range(2*n):
-    if 2*n % (i+1) == 0:
-        list.append(i+1)
-for j in range(len(list)):
-    if list[j]-list[j-1]==1:
-        print(list[j])
 
+a=0
+while a*(a+1)/2 <= n:
+    a+=1
+
+print(a-1)
