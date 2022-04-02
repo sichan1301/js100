1ef3639de86b53409ebb9165784c600030922a0d
sichan <sichan1301@naver.com>
2022-04-02 23:59:29 +0900

    vector.py update

--- practice/vector.py
+++ practice/vector.py
@@ -1,6 +1,6 @@
-v = []
-v.append((123,456))
-v.append((789,987))
+list = []
+list.append((123,456))
+list.append((789,987))
 
-print("Size : ", len(v))
-print(v)
+print("Size : ", len(list))
+print(list)
