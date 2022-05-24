80b10b9c99246e849aabcda5907d6bfbcaf35b58
sichan <sichan1301@naver.com>
2022-05-24 22:49:08 +0900

    Add Programmers 기능개발.py

---
+++ programmers/스택,큐/기능개발.py
@@ -0,0 +1,13 @@
+import math
+def solution(progresses, speeds):
+    answer = []
+    index = 0
+    list = []
+    for i in range(len(progresses)):
+        list.append(math.ceil(100-progresses[i])/speeds[i])
+    for i in range(len(list)):
+        if list[index]<list[i]:
+            answer.append(i-index)
+            index = i
+    answer.append(len(list)-index)
+    return answer
