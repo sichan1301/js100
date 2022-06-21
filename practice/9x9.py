b78f36435a46e0384ec2a9d946c40894a614dc7c
sichan <sichan1301@naver.com>
2022-06-21 23:45:08 +0900

    Add Practice math.py

--- practice/9x9.py
+++ practice/9x9.py
@@ -1,1 +1,10 @@
-print([f' {i} x {j} = {i*j}' for i in range(2,10) for j in range(1,10)])
+# print([f' {i} x {j} = {i*j}' for i in range(2,10) for j in range(1,10)])
+
+n = int(input())
+def gugu(n):
+  for i in range(1,n+1):
+    print("\n")
+    for j in range(1,10):
+      print(f'{i}*{j}={i*j}',end='\t')
+
+print(gugu(6))
---
+++ practice/math.py
@@ -0,0 +1,25 @@
+import math
+
+print(math.pow(3,2))
+# 3의 2승
+
+print(math.sqrt(25))
+# 25의 제곱근
+
+print(math.ceil(3.14))
+# 올림
+
+print(math.floor(3.78))
+# 내림
+
+print(math.trunc(3.14))
+# 0을 향하여 내림
+
+print(math.copysign(3.14, -3))
+# 두 번째 인자의 부호를 첫 번째 인자의 부호에 적용
+
+print(math.fabs(-3.14))
+# 절댓값
+
+print(math.factorial(5))
+# 팩토리얼
